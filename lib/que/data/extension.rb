8625524726ad11b24bb737696244f35b598a4edf
Nathan Palmer <nathan@nathanpalmer.com>
2017-08-06 13:58:07 -0400

    Adding an update_data class method

--- lib/que/data/extension.rb
+++ lib/que/data/extension.rb
@@ -1,36 +1,57 @@
 module Que
   module Data
     module Extension
-      def retrieve_data
-        if attrs[:job_id].nil?
-          raise "Unable to retrieve data without a job_id"
+      module ClassMethods
+        def retrieve_data(job_id)
+          if job_id.nil?
+            raise "Unable to retrieve data without a job_id"
+          end
+
+          results = Que.execute(Que::Data::SQL[:get], [job_id])
+          if results.size != 1
+            raise "Unable to find a data result"
+          else
+            
+          end
+          JSON.parse(results.first[:data])
         end
 
-        results = Que.execute(Que::Data::SQL[:get], [attrs[:job_id]])
-        if results.size != 1
-          raise "Unable to find a data result"
-        else
-          
+        def update_data(job_id, value, section: nil, property: nil)
+          if job_id.nil?
+            raise "Unable to update data without a job_id"
+          end
+
+          if section.nil?
+            Que.execute(Que::Data::SQL[:update], [job_id, value])
+            return
+          end
+
+          if property.nil?
+            Que.execute(Que::Data::SQL[:update_section], [job_id, "{#{section}}", value])
+            return
+          end
+
+          Que.execute(Que::Data::SQL[:update_section_property], [job_id, section, property, value])
         end
-        JSON.parse(results.first[:data])
       end
 
-      def update_data(value, section: nil, property: nil)
-        if attrs[:job_id].nil?
-          raise "Unable to update data without a job_id"
-        end
+      def self.prepended(base)
+        class << base
+          prepend ClassMethods
+        end  
+      end
 
-        if section.nil?
-          Que.execute(Que::Data::SQL[:update], [attrs[:job_id], value])
-          return
-        end
+      def retrieve_data
+        self.class.retrieve_data(attrs[:job_id])
+      end
 
-        if property.nil?
-          Que.execute(Que::Data::SQL[:update_section], [attrs[:job_id], "{#{section}}", value])
-          return
-        end
+      def update_data(*args)
+        self.class.update_data(attrs[:job_id], **args)
+      end
 
-        Que.execute(Que::Data::SQL[:update_section_property], [attrs[:job_id], section, property, value])
+      def _run(*args)
+        @data = retrieve_data
+        super
       end
     end
   end
